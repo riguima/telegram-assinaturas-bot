2702690f4f7799b299b2b656735950d63798b45f
riguima <richard.alexsander.guima@gmail.com>
2024-05-16 10:46:13 -0300

    feat: add validation for accounts number

--- telegram_assinaturas_bot/utils.py
+++ telegram_assinaturas_bot/utils.py
@@ -15,12 +15,15 @@ def get_plans_reply_markup(action, *args):
     reply_markup = {}
     with Session() as session:
         for plan_model in session.scalars(select(Plan)).all():
-            reply_markup[
-                f'{plan_model.name} - {plan_model.days} Dias - R${plan_model.value:.2f}'.replace(
-                    '.', ','
-                )
-            ] = {
-                'callback_data': ':'.join([action, str(plan_model.id), *args])
-            }
+            if plan_model.accounts_number < len(plan_model.signatures):
+                reply_markup[
+                    f'{plan_model.name} - {plan_model.days} Dias - R${plan_model.value:.2f}'.replace(
+                        '.', ','
+                    )
+                ] = {
+                    'callback_data': ':'.join(
+                        [action, str(plan_model.id), *args]
+                    )
+                }
     reply_markup['Voltar'] = {'callback_data': 'return_to_main_menu'}
     return reply_markup
